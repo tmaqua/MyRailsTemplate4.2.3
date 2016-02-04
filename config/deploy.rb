71c5ec31734bb8baa79ca4d4d48c54771d1204fc
murakami <tmaqua007@gmail.com>
2016-02-04 17:07:04 +0900

    rbenv path changed

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :keep_releases, 5
 
 set :rbenv_ruby, '2.2.0'
-set :rbenv_path, '/home/fankami/.rbenv/version'
+set :rbenv_path, '/home/fankami/.rbenv'
 
 namespace :deploy do
   desc 'Restart application'
