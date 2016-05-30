90ea836c4a3855e4f4ebfca1450e7d552bd5f157
murakami <tmaqua007@gmail.com>
2016-05-30 23:20:32 +0900

    add capistrano files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,13 @@
-# config valid only for current version of Capistrano
+require 'dotenv'
+require 'aws-sdk'
+Dotenv.load
+
 lock '3.4.0'
 
-set :application, 'RailsTemplate'
-set :repo_url, 'git@github.com:tmaqua/MyRailsTemplate4.2.3.git'
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :branch, 'master'
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
-set :deploy_to, '/var/www/project/RailsTemplate'
+set :application, ENV["APPLICATION_NANE"]
+set :repo_url, ENV["GIT_REPO"]
+set :branch, "master"
+set :deploy_to, ENV["DEPLOY_TO"]
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -25,54 +22,73 @@ set :deploy_to, '/var/www/project/RailsTemplate'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-# set :linked_files, fetch(:linked_files, []).push('config/settings/production.yml')
+set :linked_files, fetch(:linked_files, []).push('.env')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-# Default value for keep_releases is 5
 set :keep_releases, 5
 
 set :rbenv_ruby, '2.2.0'
-set :rbenv_path, '/home/fankami/.rbenv'
+set :rbenv_type, :user
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, "config/unicorn.conf.rb"
+
+set :aws_region, ENV['AWS_REGION']
+set :base_ami_name, ENV['BASE_AMI_NAME']
+set :keep_amis, 3 
+set :aws_access_key_id, ENV['AWS_ACCESS_KEY_ID']
+set :aws_secret_access_key, ENV['AWS_SECRET_ACCESS_KEY']  
 
 namespace :deploy do
+
+  desc 'Upload env'
+  task :upload do
+    on roles(:app) do |host|
+      upload!('.env', "#{shared_path}/.env")
+    end
+  end
+
   desc 'Restart application'
   task :restart do
-    invoke 'unicorn:restart'
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'unicorn:restart'
+    end
   end
-  desc 'Create database'
+
+  desc 'db_seed must be run only one time right after the first deploy'
+  task :db_seed do
+    on roles(:db) do |host|
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, 'db:seed'
+        end
+      end
+    end
+  end
+
+  desc 'db_create must be run only one time right after the first deploy'
   task :db_create do
     on roles(:db) do |host|
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:create'
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, 'db:create'
         end
       end
     end
   end
-  desc 'Run seed'
-  task :seed do
-    on roles(:app) do
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:seed'
-        end
-      end
-    end
-  end
+
   after :publishing, :restart
+
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
     end
   end
 end
